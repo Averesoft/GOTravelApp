673cf704a0fd7fdf29268de3ef10c9115554fe5b
Jeremy Liang <59520795+Averesoft@users.noreply.github.com>
2024-01-06 12:04:53 -0500

    Update RealTimeTrains.tsx

--- components/Train.tsx
+++ components/Train.tsx
@@ -1,99 +1,27 @@
-
 import * as React from 'react';
-import {
-    FlatList,
-    SafeAreaView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-} from 'react-native';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-type TrainLineData = {
-    id: string;
-    line: string;
-    remainingTime: string;
-    start: string;
-    end: string;
-    currentStop: string;
-    nextStop: string;
-
-};
-
-type Trainprops = {
-    trainLine: TrainLineData;
-    backgroundColor: string;
-    textColor: string;
-}
-const bull = (
-    <Box
-      component="span"
-      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-    >
-      •
-    </Box>
-  );
-const trainLine = ({trainLine}: Trainprops) =>(
-    <Card sx={{ maxWidth: 600 }}>
-        <CardContent>
-            <Typography variant='h3'  gutterBottom>
-                A Simple Material UI Card
-            </Typography>
-            <Typography variant="h4" component="div">
-                Heading
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                describes the heading
-            </Typography>
-            <Typography variant="body1">
-                Card content
-                <br />
-                {'"describes the content"'}
-            </Typography>
-        </CardContent>  
-    </Card>
-);
-
-export default function Train() {
+export default function Train({line, currStop, nextStop}:{line: string, currStop: string, nextStop: string  }) {
     return (
-        <div style={{margin: '25%'}}>
-
-        <Card sx={{ maxWidth: 600 }}>
+        <Card sx={{ maxWidth: 345 }}>
             <CardContent>
-                <Typography variant='h3'  gutterBottom>
-                    A Simple Material UI Card
-                </Typography>
-                <Typography variant="h4" component="div">
-                    Heading
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                    describes the heading
-                </Typography>
-                <Typography variant="body1">
-                    Card content
-                    <br />
-                    {'"describes the content"'}
-                </Typography>
-            </CardContent>  
+            <Typography gutterBottom variant="h5" component="div">
+                Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over 6,000
+                species, ranging across all continents except Antarctica
+            </Typography>
+            </CardContent>
+            <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+            </CardActions>
         </Card>
-        </div>
-
     );
 }
-
-
-const styles = StyleSheet.create({
-    container: {
-      display: 'flex',
-      flexWrap: 'wrap'
-    },
-    spacious: {
-      padding: 10
-    },
-});
--- screens/RealTimeTrains.tsx
+++ screens/RealTimeTrains.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StyleSheet} from 'react-native';
-import {Text, View} from 'react-native';
+import {Text, View, ScrollView} from 'react-native';
 import apiRequest from '../lib/apiRequest';
 import Train from '../components/Train';
 
@@ -9,6 +9,7 @@ export default function RealTimeTrains() {
   let trains: any;
   let [realTime, updateRealTime] = React.useState('Finding State')
   
+
   apiRequest(`api/V1/ServiceataGlance/Trains/All`, '', 'GET').then(res => {
     if (res.success) {
       trains = JSON.parse(res.response);
@@ -27,65 +28,11 @@ export default function RealTimeTrains() {
   })
   
   return (
-    <View style={styles.timeTableContainer}>
-      <View style={styles.courseContainer}>
+    <ScrollView>
 
-      {/* --- COURSE TEXT --- */}
-      <Text>{realTime}</Text>
-      <Train/>
-      </View>
-    </View>
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  course: {
-    width: "90%",
-    fontSize: 40,
-    fontWeight: "bold",
-    textAlign: "center",
-
-    fontFamily: "poppins",
-  },
-
-  timeText: {
-    width: "80%",
-    fontSize: 20,
-    textAlign: "center",
-  },
-
-  timeTableContainer: {
-    backgroundColor: "transparent",
-    position: "absolute",
-    left: 10,
-    bottom: 10,
-  },
-
-  courseContainer: {
-    backgroundColor: "transparent",
-    borderColor: "rgb(58, 106, 150)",
-    borderLeftWidth: 4,
-    alignItems: "center",
-  },
-
-  weekText: {
-    fontSize: 40,
-    fontWeight: "bold",
-    fontFamily: "poppins",
-  },
-
-  courseText: {
-    marginVertical: 5,
-    marginLeft: 10,
-    textAlign: "justify",
-    justifyContent: 'space-evenly',
-    fontSize: 17,
-  },
-
 });
